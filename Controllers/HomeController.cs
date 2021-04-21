cebb18d59640fb47f585dd875a271505a71fc026
Kirill <kirill.kanner@gmail.com>
2021-04-21 23:47:15 +0500

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VendingMachine.Models;
+
+namespace VendingMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        DrinkModelContext Context = new DrinkModelContext();
+        public ActionResult Index()
+        {
+            return View(Context.DrinkModel);
+        }
+        public ActionResult AdminPanel()
+        {
+            return View(Context.DrinkModel);
+        }
+    }
+}
---
+++ Models/DrinkModelContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace VendingMachine.Models
+{
+    public class DrinkModelContext : DbContext
+    {
+        public DbSet<DrinkModel> DrinkModel { get; set; }
+    }
+}
