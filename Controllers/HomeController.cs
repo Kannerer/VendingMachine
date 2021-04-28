f8381bef15520cdc5b5dfbe2689e001674651084
Kirill <kirill.kanner@gmail.com>
2021-04-28 15:13:29 +0500

    Final commit.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using VendingMachine.Models;
+using Newtonsoft.Json;
 
 namespace VendingMachine.Controllers
 {
@@ -28,31 +29,63 @@ namespace VendingMachine.Controllers
         }
 
         [HttpPost]
-        public ActionResult BuyDrink (int drink_id)
+        public ActionResult BuyDrink (int drink_id, int sum)
         {
             var drink = Context.DrinkModel.Where(d => d.Id== drink_id).FirstOrDefault();
-            drink.Count--;
-            if (drink.Count <= 0)
+            if (sum >= drink.Price)
             {
-                drink.Available = false;
+                drink.Count--;
+                if (drink.Count <= 0)
+                {
+                    drink.Available = false;
+                }
+                Context.SaveChanges();
             }
-            Context.SaveChanges();
+
             return Json(drink);
         }
 
         [HttpPost]
+        public ActionResult GetChange(int change)
+        {
+            var sum = 0;
+            var coins = new Dictionary<int, int>(4);
+            coins.Add(10, 0);
+            coins.Add(5, 0);
+            coins.Add(2, 0);
+            coins.Add(1, 0);
+            foreach (var coin in coins.Keys.ToList())
+            {
+                while (sum + coin <= change)
+                {
+                    sum += coin;
+                    coins[coin] += 1;
+                }
+            }
+            return Json(JsonConvert.SerializeObject(coins));
+        }
+            [HttpPost]
         public ActionResult UpdateDrink(int drink_id, string new_name, int new_price, int new_count, bool new_available)
         {
 
             var drink = Context.DrinkModel.Where(d => d.Id == drink_id).FirstOrDefault();
-
-            drink.Name = new_name;
-            drink.Price = new_price;
-            drink.Count = new_count;
-            drink.Available = new_available;
-
-            Context.SaveChanges();
-            return Json("Drink updated successfully");
+            if (new_name != "" && new_price > 0 && new_count >= 0)
+            {
+                drink.Name = new_name;
+                drink.Price = new_price;
+                drink.Count = new_count;
+                if (new_count == 0)
+                {
+                    drink.Available = false;
+                }
+                else
+                {
+                    drink.Available = new_available;
+                }
+                Context.SaveChanges();
+                return Json("Drink updated");
+            }
+            return Json(new { status = "error", message = "Указаны некорректные данные" });
         }
 
         [HttpPost]
@@ -65,12 +98,25 @@ namespace VendingMachine.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddDrink(string new_name, int new_price, int new_count, bool new_available)
+        public ActionResult UpdateCoin(int nomination, bool new_available)
         {
-            var drink = new DrinkModel { Name = new_name, Price = new_price, Count = new_count, Available = new_available };
-            Context.DrinkModel.Add(drink);
+            var coin = Context.CoinModel.Where(c => c.Nomination == nomination).FirstOrDefault();
+            coin.Available = new_available;
             Context.SaveChanges();
-            return Json(drink);
+            return Json("Coin updated");
+        }
+
+        public ActionResult Drinks(string new_name, int new_price, int new_count, bool new_available)
+        {
+            if (new_name != "" && new_price > 0 && new_count >= 0)
+            {
+                var drink = new DrinkModel { Name = new_name, Price = new_price, Count = new_count, Available = new_available };
+                Context.DrinkModel.Add(drink);
+                Context.SaveChanges();
+                ViewBag.item = drink;
+                return PartialView("_Drinks", ViewBag.item);
+            }
+            return Json(new { status = "error", message = "Указаны некорректные данные" });
         }
     }
 }
